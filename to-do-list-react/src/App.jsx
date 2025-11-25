4a402c293687a9fff204fa393684f93f7b83ab54
loganyeh <yehlogan@gmail.com>
2025-11-25 18:30:23 -0500

    clean

--- to-do-list-react/src/App.jsx
+++ to-do-list-react/src/App.jsx
@@ -3,19 +3,25 @@ import { MyContext } from "./components/MyContext"
 import AddTask from "./components/AddTask"
 
 function App() {
+  const [list, setList] = useState(["task1", "task2"]);
+  
+
+  const clearConsoleFn = () => {
+    console.clear();
+  };
 
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center">
         <div className="h-full w-1/2 flex flex-col items-center border-2 border-red-600">
-          <div className="text-3xl mb-12">TO DO LIST</div>
+          <MyContext.Provider value={{list, setList}}>
+          <div onClick={clearConsoleFn} className="text-3xl mb-12">TO DO LIST</div>
           <div className="h-1/12 w-1/2 flex justify-between">
             <input type="text" className="h-full w-8/12 border-2 border-black text-xl rounded-xl" />
-            <button className="h-full w-3/12 border-2 border-black text-2xl rounded-4xl hover:bg-gray-400 active:bg-gray-300 cursor-pointer">Submit</button>
+            <button onClick={""} className="h-full w-3/12 border-2 border-black text-2xl rounded-4xl hover:bg-gray-400 active:bg-gray-300 cursor-pointer">Submit</button>
           </div>
-          <AddTask />
-          <AddTask />
-
+            <AddTask />
+          </MyContext.Provider>
         </div>
       </div>
     </>
--- to-do-list-react/src/components/AddTask.jsx
+++ to-do-list-react/src/components/AddTask.jsx
@@ -1,10 +1,19 @@
+import { useState, useRef, useEffect, useContext } from "react";
+import { MyContext } from "./MyContext";
 
 function AddTask(){
+    const {list, setList} = useContext(MyContext);
+
+    const handleClick = () => {
+        
+    }
 
     return(
         <>
             <div className="h-1/8 w-11/12 flex justify-around items-center m-4 border-2 border-black">
-                <div className="h-full w-2/3 border-2 border-red-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro vel quo accusamus reprehenderit consequuntur consequatur quas nesciunt, veniam dolorem nihil nostrum rerum qui perspiciatis possimus, sed vitae non tenetur nobis!</div>
+                <div className="h-full w-2/3 border-2 border-red-600">
+                    {list[0]}
+                </div>
                 <button className="h-1/2 w-2/12 border-2 border-red-600 text-2xl rounded-2xl hover:bg-gray-400 active:bg-gray-300">Delete</button>
             </div>
         </>
